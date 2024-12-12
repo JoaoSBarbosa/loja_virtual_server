b2b2fe4540793aed1a87e8b28795c02c56083b8e
João Barbosa <contato.jsbarbosa@gmail.com>
2024-12-11 22:16:15 -0300

    :sparkles: feat: Implemented AccountPay and enum AccountPaySatus

---
+++ src/main/java/com/barbosacode/lojavirtual/enums/StatusContaPagar.java
@@ -0,0 +1,27 @@
+package com.barbosacode.lojavirtual.enums;
+
+public enum StatusContaPagar {
+
+    COBRANCA("Pagar"),
+    VENCIDA("Vencida"),
+    ABERTA("Aberta"),
+    QUITADA("Quitada"),
+    NEGOCIADA("Renegociada"),
+    CANCELADA_PARCIALMENTE("Cancelada Parcialmente");
+
+
+    private String descricao;
+
+    private StatusContaPagar(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString(){
+        return this.descricao;
+    }
+}
---
+++ src/main/java/com/barbosacode/lojavirtual/models/ContaPagar.java
@@ -0,0 +1,67 @@
+package com.barbosacode.lojavirtual.models;
+
+import com.barbosacode.lojavirtual.enums.StatusContaPagar;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "conta_pagar")
+@SequenceGenerator(name = "seq_conta_pagar", sequenceName = "seq_conta_pagar", allocationSize = 1, initialValue = 1)
+public class ContaPagar implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue( strategy = GenerationType.SEQUENCE, generator = "seq_conta_pagar")
+    private Long id;
+    private BigDecimal valorTotal;
+    private BigDecimal valorDesconto;
+    private BigDecimal valorPago;
+    private String descricao;
+    @Temporal(TemporalType.DATE)
+    @Column(name = "data_vencimento")
+    private Date dataVencimento;
+    @Column(name = "data_pagamento")
+    @Temporal(TemporalType.DATE)
+    private Date dataPagamento;
+
+    @Enumerated(EnumType.STRING)
+    private StatusContaPagar status;
+
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(
+            name = "id_pessoa",
+            nullable = false,
+            foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "conta_pagar_pessoa_fk"))
+    private Pessoa pessoa;
+
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(
+            name = "id_fornecedor",
+            nullable = false,
+            foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "conta_pagar_fornecedor_fk"))
+    private Pessoa fornecedor;
+
+    @ManyToOne(targetEntity = FormaPagamento.class)
+    @JoinColumn(
+            name = "id_forma_pagamento",
+            nullable = false,
+            foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "conta_pagar_forma_pagamento_fk"))
+    private FormaPagamento formaPagamento;
+
+
+
+}
