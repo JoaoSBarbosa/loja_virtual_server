4c9d287ff4a1539d278dbe9b6a03269fe8a23582
João <contato.jsbarbosa@gmail.com>
2024-12-23 21:25:53 -0300

    :sparkles: feat: Implementation annotacions in Application class; packages: Repositories,Services and Controllers

--- src/main/java/com/barbosacode/lojavirtual/LojaVirtualServerApplication.java
+++ src/main/java/com/barbosacode/lojavirtual/LojaVirtualServerApplication.java
@@ -1,9 +1,14 @@
 package com.barbosacode.lojavirtual;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.barbosacode.lojavirtual.models") // Informa onde estão as models
+@EnableJpaRepositories(basePackages = {"com.barbosacode.lojavirtual.repositories"}) // Corrigido
+@EnableTransactionManagement // Para gerenciar transações com o banco de dados
 public class LojaVirtualServerApplication {
 
 	public static void main(String[] args) {
