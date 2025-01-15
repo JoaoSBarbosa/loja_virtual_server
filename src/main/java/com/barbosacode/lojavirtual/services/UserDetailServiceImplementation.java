8b258c40974b52ecdf27d2e17c036df5fa26660b
João <contato.jsbarbosa@gmail.com>
2025-01-15 20:43:12 -0300

    :sparkles: feat: Implementation UserRepository and UserService with UserService implementation for authentication

---
+++ src/main/java/com/barbosacode/lojavirtual/services/UserDetailServiceImplementation.java
@@ -0,0 +1,26 @@
+package com.barbosacode.lojavirtual.services;
+
+import com.barbosacode.lojavirtual.models.Usuario;
+import com.barbosacode.lojavirtual.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailServiceImplementation implements UserDetailsService {
+
+    @Autowired private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findUserByLogin(login);
+
+        if(usuario == null) {
+            throw new UsernameNotFoundException("Usuário inexistente com o login informado: " + login);
+        }
+        return new User(usuario.getLogin(), usuario.getPassword(), usuario.getAuthorities());
+    }
+}
